92fa5de9fd7a6404494e92bc234e4ddf4fba5741
Liam Hood <laser4716@gmail.com>
2022-03-23 21:39:28 -0500

    worked on hw2

--- 707/hw2.m
+++ 707/hw2.m
@@ -1,6 +1,7 @@
 clear; close all; clc;
 
 problem1()
+problem1a()
 
 problem2()
 
@@ -19,23 +20,49 @@ function problem1()
     cont_matrix = [B, A*B, A^2*B];
     fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", [rank(cont_matrix), rank(A)])
     
-    des_lambda = [ -2+2i, -2-2i, -15];
+    lam1 = -2+2i;
+    des_lambda = [ lam1, conj(lam1), -15];
     I3 = eye(3);
-    D = [1, 0, 0;
-        0, 1, 0;
-        0, 0, 1];
-    des_vec = [0.20 - .35i, -.98 - .07i, 0;
-               0.20 + .35i, -.98 + .07i, 0;
-               0,           0,           1]';
+    D = diag([1, 1, 1]);
+    vec1 = [0.20 + .35i, -.98 + .07i, 0]';
+    des_vec = [vec1, conj(vec1), [0; 0; 1]];
     uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, zeros(3,2)])*[0;0;0;vec];
-%     des_lambda = [ -2+2i, -2-2i, -15];
-%     I3 = eye(3);
-%     D = [1, 0, 0;
-%         0, 1, 0];
-%     des_vec = [0.20 + .35i, -.98 + .07i, 0;
-%                0.20 - .35i, -.98 - .07i, 0;
-%                0,           0,           1]';
-%     uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, zeros(2,2)])*[0;0;vec];
+    for ii = 1:3
+        uvii = uv(des_lambda(ii), des_vec(:,ii));
+        vd(:,ii) = uvii(1:3);
+        ud(:,ii) = uvii(4:5);
+    end
+    K = ud*inv(vd);
+    fprintf("[\t%f + %fi,\t%f + %fi,\t%f + %fi\n",real(K(1,:)), imag(K(1,:)))
+    fprintf(" \t%f + %fi,\t%f + %fi,\t%f + %fi]\n",real(K(2,:)), imag(K(2,:)))
+    [vec, poles] = eig(A-B*K);
+    fprintf("Closed Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
+        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
+        real(poles(3,3)), imag(poles(3,3))])
+end
+
+function problem1a()
+    fprintf("\nProblem 1:\n")
+    A = [-1.10188, 0.90528, -.00212;
+            4.0639, -.77013, -.169190;
+            0, 0, 10];
+    B = [0, 0; 0, 1; 10, 0];
+    sys = ss(A, B, eye(3), 0);
+    [vec, poles] = eig(A);
+   fprintf("Open Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
+        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
+        real(poles(3,3)), imag(poles(3,3))])
+
+    cont_matrix = [B, A*B, A^2*B];
+    fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", [rank(cont_matrix), rank(A)])
+    
+    lam1 = -2+2i;
+    des_lambda = [ lam1, conj(lam1), -15];
+    I3 = eye(3);
+    D = diag([1, 1, 1]);
+    vec1 = [0.20 + .35i, -.98 + .07i, 0]';
+    des_vec = [vec1, conj(vec1), [0; 0; 1]];
+    uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, zeros(3,2)])*[0;0;0;vec];
     for ii = 1:3
         uvii = uv(des_lambda(ii), des_vec(:,ii));
         vd(:,ii) = uvii(1:3);
@@ -68,20 +95,14 @@ function problem2()
     cont_matrix = [B, A*B, A^2*B];
     fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", [rank(cont_matrix), rank(A)])
     
-    des_lambda = [ -2+2i, -2-2i];
+    lam1 = -2+2i;
+    des_lambda = [ lam1, conj(lam1)];
     I3 = eye(3);
-%     D = [1, 0, 0;
-%         0, 1, 0;
-%         0, 0, 1];
-%     O22 = zeros(3,2);
-%     des_vec = [0.20 - .35i, -.98 - .07i, 0;
-%                0.20 + .35i, -.98 + .07i, 0]';
-%     uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, O22])*[0;0;0;vec];
     D = [1, 0, 0;
-        0, 1, 0];
+         0, 1, 0];
     O22 = zeros(2,2);
-    des_vec = [0.20 + .35i, 0.20 - .35i;
-               -.98 + .07i, -.98 - .07i];
+    vec1 = [.2+.35i, -.98+.07i]';
+    des_vec = [vec1, conj(vec1)];
     uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, O22])*[0;0;0;vec];
     for ii = 1:2
         uvii = uv(des_lambda(ii), des_vec(:,ii));
