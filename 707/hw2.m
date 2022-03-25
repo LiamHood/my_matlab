4d144b767dc457cd38d92e7801d51af68f9caf0b
Liam Hood <laser4716@gmail.com>
2022-03-24 22:08:29 -0500

    finished hw2 707 work

--- 707/hw2.m
+++ 707/hw2.m
@@ -1,81 +1,37 @@
 clear; close all; clc;
 
 problem1()
-% problem1a()
 
-% problem2()
+problem2()
+
+problem3()
+
+problem4()
 
 %% Functions
+
 function problem1()
     fprintf("\nProblem 1:\n")
     A = [-1.10188, 0.90528, -.00212;
-            4.0639, -.77013, -.169190;
-            0, 0, 10];
-    B = [0, 0; 0, 1; 10, 0];
-    [vec, poles] = eig(A);
-   fprintf("Open Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
+           4.0639, -.77013, -.169190;
+            0,           0,       10];
+    B = [0, 0;
+         0, 1;
+         10, 0];
+    C = eye(3);
 
-    cont_matrix = [B, A*B, A^2*B];
-    fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", [rank(cont_matrix), rank(A)])
-    
     lam1 = -2+2i;
-    des_lambda = [ lam1, conj(lam1), -15];
-    I3 = eye(3);
-    D = diag([1, 1, 1]);
+    lam2 = conj(lam1);
+    lam3 = -15;
+    des_poles = [lam1, lam2, lam3];
+
     vec1 = [0.20 - .35i, -.98 - .07i, 0]';
-    des_vec = [vec1, conj(vec1), [0; 0; 1]];
-    uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, zeros(3,2)])*[0;0;0;vec];
-    for ii = 1:3
-        uvii = uv(des_lambda(ii), des_vec(:,ii));
-        vd(:,ii) = uvii(1:3);
-        ud(:,ii) = uvii(4:5);
-    end
-    K = ud*inv(vd);
-    fprintf("[\t%f + %fi,\t%f + %fi,\t%f + %fi\n",real(K(1,:)), imag(K(1,:)))
-    fprintf(" \t%f + %fi,\t%f + %fi,\t%f + %fi]\n",real(K(2,:)), imag(K(2,:)))
-    [vec, poles] = eig(A-B*K);
-    fprintf("Closed Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
-end
-
-function problem1a()
-    fprintf("\nProblem 1:\n")
-    A = [-1.10188, 0.90528, -.00212;
-            4.0639, -.77013, -.169190;
-            0, 0, 10];
-    B = [0, 0; 0, 1; 10, 0];
-    sys = ss(A, B, eye(3), 0);
-    [vec, poles] = eig(A);
-   fprintf("Open Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
-
-    cont_matrix = [B, A*B, A^2*B];
-    fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", ...
-        [rank(cont_matrix), rank(A)])
+    vec2 = conj(vec1);
+    vec3 = [0; 0; 1];
+    des_vec = [vec1, vec2, vec3];
     
-    lam1 = -2+2i;
-    des_lambda = [ lam1, conj(lam1), -15];
-    I3 = eye(3);
-    D = diag([1, 1, 1]);
-    vec1 = [0.20 + .35i, -.98 + .07i, 0]';
-    des_vec = [vec1, conj(vec1), [0; 0; 1]];
-    uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, zeros(3,2)])*[0;0;0;vec];
-    for ii = 1:3
-        uvii = uv(des_lambda(ii), des_vec(:,ii));
-        vd(:,ii) = uvii(1:3);
-        ud(:,ii) = uvii(4:5);
-    end
-    K = ud*inv(vd);
-    fprintf("[\t%f + %fi,\t%f + %fi,\t%f + %fi\n",real(K(1,:)), imag(K(1,:)))
-    fprintf(" \t%f + %fi,\t%f + %fi,\t%f + %fi]\n",real(K(2,:)), imag(K(2,:)))
-    [vec, poles] = eig(A-B*K);
-    fprintf("Closed Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
+    EigStructAssign(A, B, C, des_poles, des_vec);
+    
 end
 
 function problem2()
@@ -88,33 +44,97 @@ function problem2()
          10, 0];
     C = [1, 0, 0;
          0, 1, 0];
-    [vec, poles] = eig(A);
-    fprintf("Open Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
-
-    cont_matrix = [B, A*B, A^2*B];
-    fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n", [rank(cont_matrix), rank(A)])
     
     lam1 = -2+2i;
-    des_lambda = [ lam1, conj(lam1)];
-    I3 = eye(3);
-    D = [1, 0, 0;
+    lam2 = conj(lam1);
+    des_poles = [lam1, lam2];
+
+    vec1 = [0.20 - .35i, -.98 - .07i]';
+    vec2 = conj(vec1);
+    des_vec = [vec1, vec2];
+    
+    EigStructAssign(A, B, C, des_poles, des_vec);
+end
+
+function problem3()
+    fprintf("\nProblem 3:\n")
+    A = [-1.10188, 0.90528, -.00212;
+           4.0639, -.77013, -.169190;
+           0,      0,       10];
+    B = [0,  0, 10]';
+    C = [1, 0, 0;
          0, 1, 0];
-    O22 = zeros(2,2);
-    vec1 = [.2+.35i, -.98+.07i]';
-    des_vec = [vec1, conj(vec1)];
-    uv = @(lambda,vec) pinv([lambda.*I3 - A, B; D, O22])*[0;0;0;vec];
-    for ii = 1:2
+    
+    lam1 = -2+2i;
+    lam2 = conj(lam1);
+    des_poles = [lam1, lam2];
+
+    vec1 = [0.20 - .35i, -.98 - .07i]';
+    vec2 = conj(vec1);
+    des_vec = [vec1, vec2];
+    
+    EigStructAssign(A, B, C, des_poles, des_vec);
+end
+
+function problem4()
+fprintf("\nProblem 3:\n")
+    A = [0, 1;
+        -2, -3];
+    B = [0; 2];
+    C = [1, 0];
+    Ackermann(A, B, C);
+end
+
+function K = Ackermann(A, B, C)
+    n = length(A);
+
+    syms s
+    fdes_s = collect((s+3)*(s+5));
+    coef = sym2poly(fdes_s);
+    fdes_A = coef(1).*A*A + coef(2).*A + coef(3).*eye(n);
+
+    Con = [];
+    for ii = 1:n
+        Con = [Con, A^(ii-1)*B];
+    end
+
+    leftv = [zeros(1,n-1), 1];
+    K = leftv*inv(Con)*fdes_A;
+    fprintf("Control Gains are K = \n")
+    disp(K)
+end
+
+function K = EigStructAssign(A, B, C, des_lambda, des_vec)
+    poles = eig(A);
+    fprintf("Open Loop Poles are \\lamda = \n")
+    disp(poles)
+    
+    cont_matrix = [B, A*B, A^2*B];
+    fprintf("Controllable since controllability matrix is rank %i and A is rank %i\n\n", [rank(cont_matrix), rank(A)])
+
+    n = length(A);
+    m = size(B,2);
+    p = length(des_lambda);
+
+    I = eye(n);
+    D = diag(ones(1,p));
+    O = zeros(p,(n+m)-p);
+    top = zeros(n,1);
+    uv = @(lambda,vec) pinv([lambda.*I - A, B; D, O])*[top;vec];
+
+    for ii = 1:p
         uvii = uv(des_lambda(ii), des_vec(:,ii));
-        vd(:,ii) = uvii(1:3);
-        ud(:,ii) = uvii(4:5);
+        vd(:,ii) = uvii(1:n);
+        ud(:,ii) = uvii(n+1:end);
     end
     K = ud*inv(C*vd);
-    fprintf("[\t%f + %ii,\t%f + %ii\n",real(K(1,:)), imag(K(1,:)))
-    fprintf(" \t%f + %ii,\t%f + %ii]\n",real(K(2,:)), imag(K(2,:)))
-    [vec, poles] = eig(A-B*K*C);
-    fprintf("Closed Loop Poles are \\lamda = \n\t%f + %fi \n\t%f + %fi \n\t%f + %fi\n", ...
-        [real(poles(1,1)), imag(poles(1,1)), real(poles(2,2)), imag(poles(2,2)), ...
-        real(poles(3,3)), imag(poles(3,3))])
+    fprintf("Control Gains are K = \n")
+    disp(K)
+    poles = eig(A-B*K*C);
+    fprintf("Closed Loop Poles are \\lamda = \n")
+    disp(poles)
+    fprintf("Desired Eigen Vectors are v = \n")
+    disp(des_vec)
+    fprintf("Achievable Eigen Vectors are v = \n")
+    disp(vd)
 end
